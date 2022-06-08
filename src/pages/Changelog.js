6ce3b2e20e072b29cf6db79a079376d3d5f4433e
Raymond Lin <RLin8910@mit.edu>
2022-06-08 00:39:26 -0400

    Left Turn Legend Devlog 8

--- src/pages/Changelog.js
+++ src/pages/Changelog.js
@@ -14,6 +14,16 @@ class Changelog extends Component {
             <div className="main-body">
                 <h1>Site Changelog</h1>
                 <div className="secondary-body">
+                    <h2>June 8, 2022</h2>
+                    <ul>
+                        <li>Added "Devlog Series" section to the <a href="/left-turn-legend">Left Turn Legend</a> page.</li>
+                        <li>
+                            Updated preview video on the <a href="/left-turn-legend">Left Turn Legend</a> page
+                            to <a href="https://youtu.be/iEyottzp2Dw">Devlog 8</a>.
+                        </li>
+                    </ul>
+                </div>
+                <div className="secondary-body">
                     <h2>May 27, 2022</h2>
                     <ul>
                         <li>Added <a href="/minigames">minigames</a> page with 5 new minigames!</li>
--- src/pages/left-turn-legend/LeftTurnLegend.js
+++ src/pages/left-turn-legend/LeftTurnLegend.js
@@ -15,7 +15,7 @@ class LeftTurnLegend extends Component {
                 <h1>Left Turn Legend</h1>
                 <h2>Oval Circuit Racing, With a Twist!</h2>
                 <div className="u-16-9">
-                    <iframe src="https://youtube.com/embed/mXNs7Hg8TQk?playlist=mXNs7Hg8TQk&autoplay=1&mute=1&loop=1"></iframe>
+                    <iframe src="https://youtube.com/embed/iEyottzp2Dw?playlist=iEyottzp2Dw&autoplay=1&mute=1&loop=1"></iframe>
                 </div>
                 <div className="secondary-body">
                     <h3>Overview</h3>
@@ -26,6 +26,21 @@ class LeftTurnLegend extends Component {
                         The game focuses on intense short-track races with lots of crazy crashes while 
                         satirizing American capitalism and consumer culture. The large array of sponsors 
                         each reflect upon a real-life industry or company in a light-hearted yet meaningful manner.</p>
+                </div>
+                <div className="secondary-body">
+                    <h3>Devlog Series</h3>
+                    <p>As part of Left Turn Legend's development, I've been cataloguing entire process. My devlog series
+                       explains my problem solving process and how I approach the challenges involved in creating the game.
+                       It has also been a great point of growth for me - I've gotten a lot better at content creation and
+                       narration.
+                    </p>
+                    <p>
+                        I've also done a few 3D modeling streams, which have been a great opportunity to interact with fans
+                        while getting some work done!
+                    </p>
+                    <div className="u-16-9">
+                        <iframe src="https://www.youtube.com/embed/videoseries?list=PLvWoPUN7LnBqf0tG76klh_5G9xOvinwsS"></iframe>
+                    </div>
                 </div>
             </div>
         );
