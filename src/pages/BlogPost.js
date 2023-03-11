cd0adcc570279b812b642fd62a7904f9c987afc0
Raymond Lin <RLin8910@mit.edu>
2023-03-11 00:45:18 -0500

    Add Left Turn Legend release blog post

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -13,6 +13,7 @@ class BlogPost{
 
 const BLOG_DIR = '/blog-posts/';
 const BLOG_NAMES = [
+    'Left Turn Legend is Here!',
     'Grand Opening!',
     'How Speedrunning My Own Game Taught Me a Lesson About Floats',
     'Celebrating 2 Years of Left Turn Legend!',
