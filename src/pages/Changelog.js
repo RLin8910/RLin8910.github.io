8af20df57d3755ee9d85c1560f81d482d4534bea
Raymond Lin <RLin8910@mit.edu>
2023-03-11 00:46:42 -0500

    Update site changelog

--- src/pages/Changelog.js
+++ src/pages/Changelog.js
@@ -14,6 +14,12 @@ class Changelog extends Component {
             <div className="main-body">
                 <h1>Site Changelog</h1>
                 <div className="secondary-body">
+                    <h2>March 12, 2023</h2>
+                    <ul>
+                        <li><a href="/left-turn-legend">Left Turn Legend</a> has been released! Pages have been updated accordingly.</li>
+                    </ul>
+                </div>
+                <div className="secondary-body">
                     <h2>January 30, 2023</h2>
                     <ul>
                         <li>The <a href="/shop">Shop</a> is here! Get all-new merch for <a href="/the-street-king">The Street King</a> and <a href="left-turn-legend">Left Turn Legend</a>.</li>
